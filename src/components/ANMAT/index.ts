c4c0f310829095690637a7ae5930ee786f2b1fcc
MadRGE <furysaurio@gmail.com>
2026-01-29 19:42:52 -0300

    Add files via upload

---
+++ src/components/ANMAT/index.ts
@@ -0,0 +1,8 @@
+// ANMAT Module Components
+export { ANMATCasosList } from './ANMATCasosList';
+export { ANMATCasoCreationModal } from './ANMATCasoCreationModal';
+export { ANMATCasoDetail } from './ANMATCasoDetail';
+export { ANMATTabProductos } from './ANMATTabProductos';
+export { ANMATTabFamilias } from './ANMATTabFamilias';
+export { ANMATTabDocumentos } from './ANMATTabDocumentos';
+export { ANMATTabComunicaciones } from './ANMATTabComunicaciones';
